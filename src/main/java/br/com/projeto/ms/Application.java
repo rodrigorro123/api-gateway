e98ce8544800295449194b3f25b61e971edc8031
Rodrigo Rodrigues <rodrigo.oliveira@brmalls.com.br>
2020-10-24 05:35:20 -0300

    commit inicial

---
+++ src/main/java/br/com/projeto/ms/Application.java
@@ -0,0 +1,18 @@
+package br.com.projeto.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+//https://howtodoinjava.com/spring-cloud/spring-cloud-api-gateway-zuul/
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class Application
+{
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
